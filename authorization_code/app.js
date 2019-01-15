93982ba1828b68b76afb933b8faf89a5b89ebdbb
Jeremy Penn <jeremy.penn20@gmail.com>
2019-01-15 17:56:36 -0600

    Version 0.0.2. Fixed async issue. Began work on HTML display.

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,8 +13,8 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = ''; // Your client id
-var client_secret = ''; // Your secret
+var client_id = 'faf863c84151409b85ddf0bce2ba6a57'; // Your client id
+var client_secret = 'df4b6a122c7f4d5c8261e33e6c8171a3'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 var playlistArray = new Array();
 
@@ -92,7 +92,7 @@ app.get('/callback', function(req, res) {
 
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
-
+        var playlist_id = playlistArray;
         var options = {
           url: 'https://api.spotify.com/v1/me/playlists',
           headers: { 'Authorization': 'Bearer ' + access_token, 'Accept-Charset': 'utf-8' },
@@ -102,14 +102,36 @@ app.get('/callback', function(req, res) {
         // use the access token to access the Spotify Web API
         request.get(options, function(error, response, body) {
           //  Get list of playlist names & ids
+          getPlaylist(body, plCallback);
+        });
+
+        // Callback functions
+        function plCallback(selected_playlist) {
+          playlist_id =
+          console.log(playlist_id);
+        }
+
+        function getPlaylist(body, plCallback) {
+          // Pull user playlists
           for (var i = 0; i < body.items.length; i++) {
             playlistArray.push({'name': body.items[i].name, 'id': body.items[i].id});
           }
-        });
 
-        // Select the desired playlist to shuffle
-        //var playlist_id = playlistArray;
-        //console.log(playlist_id);
+          // prompt user to select desired playlist to shuffle
+          var selPlaylist = document.getElementById('playlist-drop');
+          for (var i = 0; i < playlistArray.length; i++) {
+            var opt = playlistArray[i].name;
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            select.appendChild(el);
+          }
+
+          // send selected playlist to Callback
+          var dropdown_return = document.getElementById('playlist-drop');
+          var selected_playlist = dropdown_return.options[dropdown_return.selectedIndex].text;
+          plCallback(selected_playlist);
+        }
 
         // Get the tracklist of the selected playlist
         /*var opts = {
