3acba866b1ce5b193f9bf38f93291adb84cc79f8
Jeremy Penn <jeremy.penn20@gmail.com>
2019-01-22 16:48:34 -0600

    Removed early comments

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -155,7 +155,6 @@ app.get('/callback', function(req, res) {
           }
         });*/
 
-        // we can also pass the token to the browser to make requests from there
         res.redirect('/#' +
           querystring.stringify({
             access_token: access_token,
